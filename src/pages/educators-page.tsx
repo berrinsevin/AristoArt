54bc3a12ddac7445d55e35f61179f37758e69ed3
berrin <berrinsevin8@gmail.com>
2025-12-25 23:28:58 +0300

    educators deleted

--- src/pages/educators-page.tsx
+++ src/pages/educators-page.tsx
@@ -1,16 +1,15 @@
 export default function Egitmenler() {
-    const images = [1,2,3,4,5,6].map(num => `/src/assets/${num}.png`);
+    // const images = [1,2,3,4,5,6].map(num => `/src/assets/${num}.png`);
     return (
         <div className="page-container fade-in">
             <h1 className="page-title">Eğitmenler</h1>
-            <p className="page-description">Sanatın farklı disiplinlerinden gelen profesyonel eğitmenlerimizle tanışın. 
-                Her biri kendi alanında deneyimli ve doğayla iç içe bir öğrenme ortamı yaratmayı amaçlıyor.
+            <p className="page-description">Program detayları ve eğitmen bilgileri yakında eklenecektir.
             </p>
-            <div className="educators-grid">
+            {/* <div className="educators-grid">
                 {images.map((src, idx) => (
                         <img src={src} alt={`Eğitmen ${idx+1}`} className="educator-img" />
                 ))}
-            </div>
+            </div> */}
         </div>
     )
 }
